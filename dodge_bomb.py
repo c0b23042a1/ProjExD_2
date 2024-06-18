bfe5192691883c626eb221c41be060ccd19d4095
c0b23042 <c0b23042a1@edu.teu.ac.jp>
2024-06-18 14:45:10 +0900

    練習３：画面内外チェック

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,19 @@ DELTA = {  # 移動量辞書
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数: こうかとんRectか爆弾Rect
+    戻り値: タプル(横方向判定結果, 縦方向判定結果)
+    画面内ならTrue, 画面外ならFalse
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: #  横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: #  縦方向判定
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -43,9 +56,18 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
         enn_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(enn_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(enn, enn_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
